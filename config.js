87901953d2e36141886546affdb4b14abf38c637
road <ryssbin@gmail.com>
2023-08-13 17:17:22 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-
 import { stringToPath } from '@cosmjs/crypto'
 
 export default {
-    port: 8088, // http port 
+    port: 5000, // http port 
     db: {
         path: "./db/faucet.db" // save request states 
     },
@@ -12,195 +11,195 @@ export default {
         deployer: `<a href="#">Ping.pub</a>`
     },
     blockchains: [
-        {
-            name: "gon-irishub-1",
-            endpoint: {
-                // make sure that CORS is enabled in rpc section in config.toml
-                // cors_allowed_origins = ["*"]
-                rpc_endpoint: " http://34.80.93.133:26657",
-            },
-            sender: {
-                mnemonic: "wise rule method circle general over tool exhibit over group nuclear meat inform rival before short inner bind short enact team dinner swift ritual",
-                option: {
-                    hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
-                    prefix: "iaa"
-                }
-            },
-            tx: {
-                amount: {
-                    denom: "uiris",
-                    amount: "10000000"
-                },
-                fee: {
-                    amount: [
-                        {
-                            amount: "5000",
-                            denom: "uiris"
-                        }
-                    ],
-                    gas: "200000"
-                },
-            },
-            limit: {
-                // how many times each wallet address is allowed in a window(24h)
-                address: 1, 
-                // how many times each ip is allowed in a window(24h),
-                // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10 
-            }
-        },
-        {
-            name: "Stargaze-elgafar-1",
-            endpoint: {
-                // make sure that CORS is enabled in rpc section in config.toml
-                // cors_allowed_origins = ["*"]
-                rpc_endpoint: "https://rpc.elgafar-1.stargaze-apis.com:443",
-            },
-            sender: {
-                mnemonic: "wise rule method circle general over tool exhibit over group nuclear meat inform rival before short inner bind short enact team dinner swift ritual",
-                option: {
-                    hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
-                    prefix: "stars"
-                }
-            },
-            tx: {
-                amount: {
-                    denom: "ustars",
-                    amount: "10000000"
-                },
-                fee: {
-                    amount: [
-                        {
-                            amount: "100000",
-                            denom: "ustars"
-                        }
-                    ],
-                    gas: "200000"
-                },
-            },
-            limit: {
-                // how many times each wallet address is allowed in a window(24h)
-                address: 1, 
-                // how many times each ip is allowed in a window(24h),
-                // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10 
-            }
-        },
-        {
-            name: "Juno-uni-6",
-            endpoint: {
-                // make sure that CORS is enabled in rpc section in config.toml
-                // cors_allowed_origins = ["*"]
-                rpc_endpoint: "https://rpc.uni.junonetwork.io/",
-            },
-            sender: {
-                mnemonic: "wise rule method circle general over tool exhibit over group nuclear meat inform rival before short inner bind short enact team dinner swift ritual",
-                option: {
-                    hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
-                    prefix: "juno"
-                }
-            },
-            tx: {
-                amount: {
-                    denom: "ujunox",
-                    amount: "10000000"
-                },
-                fee: {
-                    amount: [
-                        {
-                            amount: "100000",
-                            denom: "ujunox"
-                        }
-                    ],
-                    gas: "200000"
-                },
-            },
-            limit: {
-                // how many times each wallet address is allowed in a window(24h)
-                address: 1, 
-                // how many times each ip is allowed in a window(24h),
-                // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10 
-            }
-        },
-        {
-            name: "gon-flixnet-1",
-            endpoint: {
-                // make sure that CORS is enabled in rpc section in config.toml
-                // cors_allowed_origins = ["*"]
-                rpc_endpoint: "http://65.21.93.56:26657/",
-            },
-            sender: {
-                mnemonic: "wise rule method circle general over tool exhibit over group nuclear meat inform rival before short inner bind short enact team dinner swift ritual",
-                option: {
-                    hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
-                    prefix: "omniflix"
-                }
-            },
-            tx: {
-                amount: {
-                    denom: "uflix",
-                    amount: "10000000"
-                },
-                fee: {
-                    amount: [
-                        {
-                            amount: "100000",
-                            denom: "uflix"
-                        }
-                    ],
-                    gas: "200000"
-                },
-            },
-            limit: {
-                // how many times each wallet address is allowed in a window(24h)
-                address: 1, 
-                // how many times each ip is allowed in a window(24h),
-                // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10 
-            }
-        },
+        // {
+        //     name: "cascadia_6102-1",
+        //     endpoint: {
+        //         // make sure that CORS is enabled in rpc section in config.toml
+        //         // cors_allowed_origins = ["*"]
+        //         rpc_endpoint: " https://cscd-rpc.systemd.run:443",
+        //     },
+        //     sender: {
+        //         mnemonic: "mirror calm grace guide dismiss novel ramp bounce manage frost veteran burger group bridge ocean primary lucky thought festival siren use regret slush problem",
+        //         option: {
+        //             hdPaths: [stringToPath("m/44'/60'/0'/0/0")],
+        //             prefix: "cascadia"
+        //         }
+        //     },
+        //     tx: {
+        //         amount: {
+        //             denom: "aCC",
+        //             amount: "10000000"
+        //         },
+        //         fee: {
+        //             amount: [
+        //                 {
+        //                     amount: "7",
+        //                     denom: "aCC"
+        //                 }
+        //             ],
+        //             gas: "200000"
+        //         },
+        //     },
+        //     limit: {
+        //         // how many times each wallet address is allowed in a window(24h)
+        //         address: 1, 
+        //         // how many times each ip is allowed in a window(24h),
+        //         // if you use proxy, double check if the req.ip is return client's ip.
+        //         ip: 10 
+        //     }
+        // },
+        // {
+        //     name: "Stargaze-elgafar-1",
+        //     endpoint: {
+        //         // make sure that CORS is enabled in rpc section in config.toml
+        //         // cors_allowed_origins = ["*"]
+        //         rpc_endpoint: "https://rpc.elgafar-1.stargaze-apis.com:443",
+        //     },
+        //     sender: {
+        //         mnemonic: "wise rule method circle general over tool exhibit over group nuclear meat inform rival before short inner bind short enact team dinner swift ritual",
+        //         option: {
+        //             hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+        //             prefix: "stars"
+        //         }
+        //     },
+        //     tx: {
+        //         amount: {
+        //             denom: "ustars",
+        //             amount: "10000000"
+        //         },
+        //         fee: {
+        //             amount: [
+        //                 {
+        //                     amount: "100000",
+        //                     denom: "ustars"
+        //                 }
+        //             ],
+        //             gas: "200000"
+        //         },
+        //     },
+        //     limit: {
+        //         // how many times each wallet address is allowed in a window(24h)
+        //         address: 1, 
+        //         // how many times each ip is allowed in a window(24h),
+        //         // if you use proxy, double check if the req.ip is return client's ip.
+        //         ip: 10 
+        //     }
+        // },
+        // {
+        //     name: "Juno-uni-6",
+        //     endpoint: {
+        //         // make sure that CORS is enabled in rpc section in config.toml
+        //         // cors_allowed_origins = ["*"]
+        //         rpc_endpoint: "https://rpc.uni.junonetwork.io/",
+        //     },
+        //     sender: {
+        //         mnemonic: "wise rule method circle general over tool exhibit over group nuclear meat inform rival before short inner bind short enact team dinner swift ritual",
+        //         option: {
+        //             hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+        //             prefix: "juno"
+        //         }
+        //     },
+        //     tx: {
+        //         amount: {
+        //             denom: "ujunox",
+        //             amount: "10000000"
+        //         },
+        //         fee: {
+        //             amount: [
+        //                 {
+        //                     amount: "100000",
+        //                     denom: "ujunox"
+        //                 }
+        //             ],
+        //             gas: "200000"
+        //         },
+        //     },
+        //     limit: {
+        //         // how many times each wallet address is allowed in a window(24h)
+        //         address: 1, 
+        //         // how many times each ip is allowed in a window(24h),
+        //         // if you use proxy, double check if the req.ip is return client's ip.
+        //         ip: 10 
+        //     }
+        // },
+        // {
+        //     name: "gon-flixneta-1",
+        //     endpoint: {
+        //         // make sure that CORS is enabled in rpc section in config.toml
+        //         // cors_allowed_origins = ["*"]
+        //         rpc_endpoint: "http://65.21.93.56:26657/",
+        //     },
+        //     sender: {
+        //         mnemonic: "wise rule method circle general over tool exhibit over group nuclear meat inform rival before short inner bind short enact team dinner swift ritual",
+        //         option: {
+        //             hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+        //             prefix: "omniflix"
+        //         }
+        //     },
+        //     tx: {
+        //         amount: {
+        //             denom: "uflix",
+        //             amount: "10000000"
+        //         },
+        //         fee: {
+        //             amount: [
+        //                 {
+        //                     amount: "100000",
+        //                     denom: "uflix"
+        //                 }
+        //             ],
+        //             gas: "200000"
+        //         },
+        //     },
+        //     limit: {
+        //         // how many times each wallet address is allowed in a window(24h)
+        //         address: 1, 
+        //         // how many times each ip is allowed in a window(24h),
+        //         // if you use proxy, double check if the req.ip is return client's ip.
+        //         ip: 10 
+        //     }
+        // },
         {
             type: 'Ethermint',
             ids: {
-                chainId: 7000,
-                cosmosChainId: 'uptickd_7000-2',
+                chainId: 6102,
+                cosmosChainId: 'cascadia_6102-1',
             },
-            name: "uptickd_7000-2",
+            name: "cascadia_6102-1",
             endpoint: {
                 // make sure that CORS is enabled in rpc section in config.toml
                 // cors_allowed_origins = ["*"]
-                rpc_endpoint: "http://52.220.252.160:26657/",
-                evm_endpoint: "http://52.220.252.160:8545/",
+                rpc_endpoint: "https://cscd-rpc.systemd.run:443/",
+                evm_endpoint: "https://testnet.cascadia.foundation:443/",
             },
             sender: {
-                mnemonic: "wise rule method circle general over tool exhibit over group nuclear meat inform rival before short inner bind short enact team dinner swift ritual",
+                mnemonic: "mirror calm grace guide dismiss novel ramp bounce manage frost veteran burger group bridge ocean primary lucky thought festival siren use regret slush problem",
                 option: {
-                    hdPaths: [stringToPath("m/44'/60/0'/0/0")],
-                    prefix: "uptick"
+                    hdPaths: [stringToPath("m/44'/60'/0'/0/0")],
+                    prefix: "cascadia"
                 }
             },
             tx: {
                 amount: {
-                    denom: "auptick",
+                    denom: "aCC",
                     amount: "5000000000000000000"
                 },
                 fee: {
                     amount: [
                         {
-                            amount: "100000",
-                            denom: "auptick"
+                            amount: "7",
+                            denom: "aCC"
                         }
                     ],
-                    gas: "10000000000000"
+                    gas: "250000"
                 },
             },
             limit: {
                 // how many times each wallet address is allowed in a window(24h)
-                address: 1, 
+                address: 100, 
                 // how many times each ip is allowed in a window(24h),
                 // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10 
+                ip: 1000 
             }
         },
 
